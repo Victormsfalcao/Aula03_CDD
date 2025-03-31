dceaf3906d0ed44f9789cc0ee82e1df9940330a6
Victormsfalcao <victorfalcao782@gmail.com>
2025-03-31 15:33:51 -0300

    Quatro operações

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+num1 = float(input("Insira o primeiro numero 1: "))
+num2 = float(input("Insira o primeiro numero 2: "))
+
+adicao = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"Adição: A soma do numero 1 : {num1} com o numero 2 : {num2} resulta em: {adicao}"
+       f"\nSubtração: A subtração do numero 1 : {num1} com o numero 2 : {num2} resulta em: {subtracao}"
+      f"\nMultiplicação: A multiplicação do numero 1: {num1} com o numero 2 : {num2} resulta em: {multiplicacao}"
+      f"\nDivisão: A Divisão do numero 1: {num1} com o numero 2 : {num2} resulta em: {divisao}")
