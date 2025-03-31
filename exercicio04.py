33eff0759442ec5133bdf977f36e065f9c958e07
Victormsfalcao <victorfalcao782@gmail.com>
2025-03-31 16:13:10 -0300

    Exercicio 4 : ordem crescente

---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+num1 = int(input("Insira numero 1: "))
+num2 = int(input("Insira número 2: "))
+
+if num1 <= num2:
+  print(f"A ordem crescente dos numeros é: {num1} e {num2} ")
+
+else:
+   print(f"A ordem crescente dos numeros é: {num2} e {num1}")
