4268b7a19c0d6cc35aad9246361e4a818421f2e7
Victormsfalcao <victorfalcao782@gmail.com>
2025-03-31 14:15:02 -0300

    Primeiro commit

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome = input("Insira o seu nome: ")
+idade = int(input("Insira sua idade: "))
+salario = float(input("Insira seu salario: "))
+aumentoSalario = int(input("Insira o percentual de aumento"))
+
