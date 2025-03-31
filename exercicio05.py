09f2eea41d504b9e3519055b2e894a8784def098
Victormsfalcao <victorfalcao782@gmail.com>
2025-03-31 16:52:17 -0300

    Exercicio 5 : Calculo de media

--- exercicio05.py
+++ exercicio05.py
@@ -2,10 +2,14 @@ nota1 = float(input("Insira a primeira nota: "))
 nota2 = float(input("Insira a segunda nota: "))
 nota3 = float(input("Insira a terceira nota: "))
 
-media = (nota1 + nota2 + nota3) // 3
+media = (nota1 + nota2 + nota3) / 3
 
 if media >= 7.0:
-    print(f"Aluno aprovado, media: {media}")
+    print(f"Aluno aprovado, media: {media: .2f}")
 
 else:
-    print(f"Aluno reprovado, media: {media}")
+    if media <4:
+        print(f"Aluno reprovado, media: {media: .2f}")
+
+    else:
+     print(f"Recuperação, media: {media: .2f}")
