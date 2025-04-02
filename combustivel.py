53d879c7e0b2ecc5474f4c978dbaf6e6fa8800f9
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-02 14:22:32 -0300

    Exercicio 7 : Abastecimento - Atualização do IF ELSE

--- combustivel.py
+++ combustivel.py
@@ -1,14 +1,20 @@
-tipoCombustivel = input("Insira o tipo de combustível:")
+tipoCombustivel = input("Insira o tipo de combustível: ")
 numLitros = float(input("Insira a quantidade de litros que você quer abastecer: "))
 
 gasolina = 5.80
 etanol = 4.90
 
-valorGasolina = numLitros * gasolina
-valorEtanol = numLitros * etanol
 
 if tipoCombustivel == "G":
+
+    valorGasolina = numLitros * gasolina
     print(f"Você abasteceu {numLitros} litros de Gasolina voce pagou R${valorGasolina}. ")
 
 else:
-    print(f"Você abasteceu {numLitros} litros de Etanol voce pagou R${valorEtanol}. ")
+    if tipoCombustivel == "E":
+            valorEtanol = numLitros * etanol
+
+            print(f"Você abasteceu {numLitros} litros de Etanol voce pagou R${valorEtanol}. ")
+
+    else:
+        print("Tipo de combustivel invalido.")
