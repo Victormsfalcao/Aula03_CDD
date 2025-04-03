e33d4bbb428c7b0c8ce15b1efc8ab18933623aa0
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-03 20:38:02 -0300

    Desafio Completo :)

--- desafio.py
+++ desafio.py
@@ -1,20 +1,25 @@
-hora01 = float(input("Insira a primeira hora: "))
-minuto01 = float(input("Insira o segundo minuto: "))
-hora02 = float(input("Insira a segunda hora : "))
-minuto02 = float(input("Insira o segundo minuto : "))
+hora01 = int(input("Insira a primeira hora: "))
+minuto01 = int(input("Insira o segundo minuto: "))
+hora02 = int(input("Insira a segunda hora : "))
+minuto02 = int(input("Insira o segundo minuto : "))
 
-entrada01 = hora01 + minuto01
-entrada02 = hora02 + minuto02
+somaHoras = hora01 + hora02
+somaMinutos = minuto01 + minuto02
 
-somasHoras = hora01 + hora02
-somaMinutos = (minuto01 + minuto02) - 59
+if somaMinutos >= 60:
+    somaMinutos -= 60
+    somaHoras += 1
 
-converteHoras = somasHoras * 60
+if somaHoras > 12:
+    somaHoras -= 12
 
-reConversao = (converteHoras + somaMinutos ) /
-
-print(reConversao)
-
+print(f"O Horario é {somaHoras}:{somaMinutos:02d}")
 
 
-#
+
+
+
+
+
+
+
