dabf93336f48b90e7cd332991ea75c555f98f3de
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-07 16:22:04 -0300

    Exercicio 12: 1 a 10

--- UM A 10.py
+++ UM A 10.py
@@ -1,3 +1,3 @@
 
 for i in range (0,10,1):
-    print(i + 1)
+    print(i + 1, end=" ")
---
+++ exercicio13.py
@@ -0,0 +1,7 @@
+resultado = 0
+for i in range (1,11,1):
+    print(f"Passo Atual: {i}")
+    num = int(input("Insira um numero: "))
+    resultado = resultado + num
+
+print(resultado)
